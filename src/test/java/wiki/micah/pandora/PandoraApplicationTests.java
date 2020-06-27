2325d334ffec3ac771a650d3096639069c031abb
micah.shi@gmail.com <shijinping>
2020-06-27 15:38:31 +0800

    update

---
+++ src/test/java/wiki/micah/pandora/PandoraApplicationTests.java
@@ -0,0 +1,13 @@
+package wiki.micah.pandora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
